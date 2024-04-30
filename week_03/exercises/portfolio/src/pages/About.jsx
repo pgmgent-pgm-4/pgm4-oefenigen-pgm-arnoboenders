97ccb6458fef19792e0d4ed13d6dea6ce35501cb
pgm-arnoboenders <145023819+pgm-arnoboenders@users.noreply.github.com>
2024-04-30 11:18:37 +0200

    Centered pages

--- week_03/exercises/portfolio/src/pages/About.jsx
+++ week_03/exercises/portfolio/src/pages/About.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { Typography, Container } from "@mui/material";
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <Container maxWidth="sm">
+      <center>
+        <Typography variant="h1">About</Typography>;
+      </center>
+    </Container>
+  );
 }
