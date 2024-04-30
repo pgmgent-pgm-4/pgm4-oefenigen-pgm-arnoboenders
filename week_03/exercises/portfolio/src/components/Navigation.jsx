87a29bfe0671dd9bf2812ac2031124439a5aeb36
pgm-arnoboenders <145023819+pgm-arnoboenders@users.noreply.github.com>
2024-04-30 11:17:37 +0200

    Material ui styling for navigation

--- week_03/exercises/portfolio/src/components/Navigation.jsx
+++ week_03/exercises/portfolio/src/components/Navigation.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import {ROUTES} from "../routes/routes"
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { ROUTES } from "../routes/routes";
+import { NavLink } from "react-router-dom";
+import { Button } from "@mui/material";
 
 export default function Navigation() {
   return (
-      <div>
-          <NavLink to={ROUTES.home.path}>{ROUTES.home.title}</NavLink>
-          <NavLink to={ROUTES.about.path}>{ROUTES.about.title}</NavLink>
-          <NavLink to={ROUTES.projects.path}>{ROUTES.projects.title}</NavLink>
-          <NavLink to={ROUTES.contact.path}>{ROUTES.contact.title}</NavLink>
-          <NavLink to={ROUTES.resume.path}>{ROUTES.resume.title}</NavLink>
-      </div>
-  )
+    <div>
+      <Button variant="outlined">
+        <NavLink to={ROUTES.home.path}>{ROUTES.home.title}</NavLink>
+      </Button>
+      <Button variant="outlined">
+        <NavLink to={ROUTES.about.path}>{ROUTES.about.title}</NavLink>
+      </Button>
+      <Button variant="outlined">
+        <NavLink to={ROUTES.projects.path}>{ROUTES.projects.title}</NavLink>
+      </Button>
+      <Button variant="outlined">
+        <NavLink to={ROUTES.contact.path}>{ROUTES.contact.title}</NavLink>
+      </Button>
+      <Button variant="outlined">
+        <NavLink to={ROUTES.resume.path}>{ROUTES.resume.title}</NavLink>
+      </Button>
+    </div>
+  );
 }
