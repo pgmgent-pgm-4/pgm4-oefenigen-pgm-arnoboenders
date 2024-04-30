243524f35853263503e036e084aca71be1a75dd8
pgm-arnoboenders <145023819+pgm-arnoboenders@users.noreply.github.com>
2024-04-30 11:18:48 +0200

    Added personal info and little styling

--- week_03/exercises/portfolio/src/pages/Contact.jsx
+++ week_03/exercises/portfolio/src/pages/Contact.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import { List, ListItem } from "@mui/material";
+import React from "react";
+
+// icons
+import AlternateEmailRoundedIcon from "@mui/icons-material/AlternateEmailRounded";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Contact() {
+  const info = {
+    email: {
+      name: "email",
+      address: "arnoboenders@gmail.com",
+    },
+    github: {
+      name: "Github",
+      address: "https://github.com/Arno-Boenders",
+    },
+    linkedin: {
+      name: "LinkedIn",
+      address: "https://www.linkedin.com/in/arno-boenders-816117228/",
+    },
+  };
   return (
-    <div>Contact</div>
-  )
+    <List>
+      <ListItem>
+        <AlternateEmailRoundedIcon />
+        {info.email.name}: {info.email.address}
+      </ListItem>
+      <ListItem>
+        <GitHubIcon />
+        {info.github.name}:
+        <a href={info.github.address}>{info.github.address}</a>
+      </ListItem>
+      <ListItem>
+        <LinkedInIcon />
+        {info.linkedin.name}:
+        <a href={info.linkedin.address}>{info.linkedin.address}</a>
+      </ListItem>
+    </List>
+  );
 }
